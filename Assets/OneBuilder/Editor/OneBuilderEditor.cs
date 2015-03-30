92f27eb458b4008ccb9fb02997570675b352301b
zhaoboqiang <zhaoboqiang@queencrowd.com>
2015-03-30 20:20:16 +0800

    trivial

--- Assets/OneBuilder/Editor/OneBuilderEditor.cs
+++ Assets/OneBuilder/Editor/OneBuilderEditor.cs
@@ -73,7 +73,7 @@ namespace dpull
 
 		static BuildTarget GetIOSBuildTarget()
 		{
-			#if UNITY_4_0 || UNITY_4_0_1 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6
+			#if UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6
 			return BuildTarget.iPhone;
 			#else
 			return BuildTarget.iOS;
